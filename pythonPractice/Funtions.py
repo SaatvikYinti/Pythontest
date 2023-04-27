55f7b0052d5a1c147cf0295e45ca8fe6d1d5f2b3
Saatvik <saatvikyinti@gmail.com>
2023-04-27 18:07:32 +0530

    Python Initial Commit

---
+++ pythonPractice/Funtions.py
@@ -0,0 +1,18 @@
+#zeroparamater function
+def test():
+    print("hello world")
+test()
+
+print("------------")
+#paramater function
+
+def abc(a):
+    print(a+10)
+abc(10)
+
+print("------------")
+#optional/default paramater function
+
+def getcountryname(Name= "India"):
+    print(Name)
+getcountryname()
